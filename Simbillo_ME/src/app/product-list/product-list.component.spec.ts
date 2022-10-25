0b62a0f2006be1436673eed7914404473bbc081b
Emerson Simbillo <98303956+emersonsimbillo@users.noreply.github.com>
2022-10-25 18:32:38 +0800

    me
    me

--- Simbillo_ME/src/app/product-list/product-list.component.spec.ts
+++ Simbillo_ME/src/app/product-list/product-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductsListComponent } from './products-list.component';
+import { ProductsListComponent } from './product-list.component';
 
 describe('ProductsListComponent', () => {
   let component: ProductsListComponent;
