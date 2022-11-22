989f77a505790f5874889bb8f01c236cc3d237af
Emerson Simbillo <98303956+emersonsimbillo@users.noreply.github.com>
2022-11-22 22:24:47 +0800

    f

--- APPDEV_FINALS/src/app/app.component.spec.ts
+++ APPDEV_FINALS/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Charog_ME'`, () => {
+  it(`should have as title 'Simbillo_Finals'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Charog_ME');
+    expect(app.title).toEqual('Simbillo_Finals_ME');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Charog_ME app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Simbillo_Finals app is running!');
   });
 });
